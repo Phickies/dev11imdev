3ca6aa96577dc399fc5aa1a757be6e57ce607e3e
Phicks-debug <an.tq@techxcorp.com>
2025-10-16 04:13:29 +0200

    minor fix because im ocd

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,19 +4,19 @@ namespace Assets.Scripts
 {
     public class GameManager : MonoBehaviour
     {
-    [Header("Game Settings")]
-    public float gravity = -9.81f;
+        [Header("Game Settings")]
+        public float gravity = -9.81f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
     }
 }
